ff97f18757ee7476e3434d0543977f5a80f99409
msjtw <fiedler.stanislaw@gmail.com>
2020-11-11 15:25:37 +0100

    add OI gra v1.1

--- OI/gra/gra.cpp
+++ OI/gra/gra.cpp
@@ -1,82 +1,123 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-#include <climits>
 
 using namespace std;
 
-struct dziura{
-    bool up;
-    bool down;
-    bool down_end;
+struct poziom{
+    int bez ;
+    int koszt ;
+    int ost_dziura ;
 };
 
-dziura plansza[10010][10010];
-int odw[10010][10010];
-
-int plat, len, p;
-
-int dp(int row, int col){
-    if(col == len){
-        return 0;
-    }
-    else{
-        int wynik;
-        if(odw[row][col] == -1){
-            if(plansza[row][col].up){
-                wynik = min(dp(row-1, col+1)+1, dp(row,  col+1));
-            }
-            else if(plansza[row][col+1].down){
-                wynik = min(dp(row, col+2)+1, dp(row+1,  col+1));
-            }
-            else if(plansza[row][col+1].down_end){
-                wynik = dp(row, col+2)+1;
-            }
-            else{
-                wynik = dp(row, col+1);
-            }
-            odw[row][col] = wynik;
-        }
-        else{
-            wynik = odw[row][col];
-        }
-        return wynik;
-    }
-    
-}
+vector <vector <int>> dziury;
+vector <poziom> poziomy;
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    cin >> plat >> len >> p;
-    for(int i = 0; i < plat; i++){
-        int a;
+    int poziomow, dlugosc, p;
+    cin >> poziomow >> dlugosc >> p;
+    dziury.resize(poziomow+5);
+    poziomy.resize(poziomow+5);
+    for(int i = 1; i <= poziomow; i++){
+        int a, b;
         cin >> a;
-        while(a--){
-            int b;
+        poziomy[i].ost_dziura = a-1;
+        poziomy[i].bez = 0;
+        poziomy[i].koszt = 0;
+        for(int j = 0; j < a; j++){
             cin >> b;
-            if(a != 1){
-                plansza[i+1][b].down = true;
-                plansza[i+2][b].up = true;   
-            }
-            else{
-                plansza[i+1][b].down_end = true;
-            }
-            
+            dziury[i].push_back(b);
         }
     }
 
-    for(int i = 0; i <= plat; i++){
-        for(int j = 0; j < len; j++){
-            odw[i][j] = -1;
+    int h = 1;
+
+    poziomy[poziomow].bez = dziury[poziomow][poziomy[poziomow].ost_dziura]+1;
+
+    if(dziury[1].size() > 0){
+        for(int ak_poz = poziomow; ak_poz > 0; ak_poz--){
+            while(poziomy[ak_poz].ost_dziura >= 0 && dziury[ak_poz][poziomy[ak_poz].ost_dziura] >= poziomy[ak_poz].bez){
+                poziomy[ak_poz].ost_dziura--;
+            }
+            if(poziomy[ak_poz].ost_dziura == -1)
+                poziomy[ak_poz].bez = 0;
+            else{
+                poziomy[ak_poz].bez = dziury[ak_poz][poziomy[ak_poz].ost_dziura]+1;
+
+                if(ak_poz != poziomow){
+                    poziomy[ak_poz+1].bez = dziury[ak_poz][poziomy[ak_poz].ost_dziura];
+                    poziomy[ak_poz+1].koszt = poziomy[ak_poz].koszt + 1;
+                    while(poziomy[ak_poz+1].ost_dziura >= 0 && dziury[ak_poz+1][poziomy[ak_poz+1].ost_dziura] >= poziomy[ak_poz+1].bez)
+                        poziomy[ak_poz+1].ost_dziura--;
+                    if(poziomy[ak_poz+1].ost_dziura == -1)
+                        poziomy[ak_poz+1].bez = 0;
+                    else
+                        poziomy[ak_poz+1].bez = dziury[ak_poz+1][poziomy[ak_poz+1].ost_dziura]+1;
+                }
+                poziomy[ak_poz-1].bez = dziury[ak_poz][poziomy[ak_poz].ost_dziura];
+                while(poziomy[ak_poz-1].ost_dziura >= 0 && dziury[ak_poz-1][poziomy[ak_poz-1].ost_dziura] >= poziomy[ak_poz-1].bez)
+                    poziomy[ak_poz-1].ost_dziura--;
+                if(poziomy[ak_poz-1].ost_dziura == -1)
+                    poziomy[ak_poz-1].bez = 0;
+                else
+                    poziomy[ak_poz-1].bez = dziury[ak_poz-1][poziomy[ak_poz-1].ost_dziura]+1;
+                
+            }
+            h = ak_poz-1;
         }
+
+        do{
+            for(int ak_poz = poziomow; ak_poz > h; ak_poz--){
+                if(poziomy[ak_poz].bez > 0){
+                    poziomy[ak_poz].ost_dziura--;
+                    poziomy[ak_poz].koszt++;
+                    if(poziomy[ak_poz].ost_dziura != -1)
+                        poziomy[ak_poz].bez = dziury[ak_poz][poziomy[ak_poz].ost_dziura]+1;
+                    else 
+                        poziomy[ak_poz].bez = 0;
+                }
+                if(poziomy[ak_poz].bez > 0){
+                    poziomy[ak_poz+1].bez = poziomy[ak_poz].bez+1;
+                    while(poziomy[ak_poz+1].ost_dziura >= 0 && dziury[ak_poz+1][poziomy[ak_poz+1].ost_dziura] >= poziomy[ak_poz+1].bez){
+                        poziomy[ak_poz+1].ost_dziura--;
+                    }
+                    if(poziomy[ak_poz+1].ost_dziura == -1)
+                        poziomy[ak_poz+1].bez = 0;
+                    else
+                        poziomy[ak_poz+1].bez = dziury[ak_poz+1][poziomy[ak_poz+1].ost_dziura]+1;
+                    poziomy[ak_poz+1].koszt = poziomy[ak_poz].koszt+1;
+                    
+
+                    poziomy[ak_poz-1].bez = poziomy[ak_poz].bez-1;
+                    if(ak_poz > 1){
+                        while(poziomy[ak_poz-1].ost_dziura >= 0 && dziury[ak_poz-1][poziomy[ak_poz-1].ost_dziura] >= poziomy[ak_poz-1].bez){
+                            poziomy[ak_poz-1].ost_dziura--;
+                        }
+                        if(poziomy[ak_poz-1].ost_dziura == -1)
+                            poziomy[ak_poz-1].bez = 0;
+                        else
+                            poziomy[ak_poz-1].bez = dziury[ak_poz-1][poziomy[ak_poz-1].ost_dziura]+1;
+                    }
+                } 
+            }
+        }while(poziomy[poziomow].bez > 0);
     }
+
+    
+
+    // for(int i = 1; i <= poziomow; i++){
+    //     cout << poziomy[i].bez << '\t' << poziomy[i].koszt << '\t' << poziomy[i].ost_dziura << endl;
+    // }
+    // cout << h;
 
     while(p--){
         int a;
         cin >> a;
-        cout << dp(a, 1) << endl;
+        cout << poziomy[a].koszt << '\n';
     }
     return 0;
 }
